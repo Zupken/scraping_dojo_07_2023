78d4b6a63d953d07c16cb9c75e4be2f004d79029
zupkenpwr <259455@student.pwr.edu.pl>
2023-07-07 13:35:05 +0200

    Refactoring.

--- run.py
+++ run.py
@@ -2,19 +2,18 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
 from dotenv import load_dotenv
 import os
 import json
+from time import sleep
 
-# load .env file
+
 load_dotenv()
-
-PROXY = os.getenv("PROXY")
 
 
 def write_to_jsonl(filename, data):
+    if os.path.exists(filename):
+        os.remove(filename)
     with open(filename, 'a', encoding='utf-8') as f:
         for entry in data:
             f.write(json.dumps(entry, ensure_ascii=False) + '\n')
@@ -25,32 +24,42 @@ class Scraping:
     def __init__(self):
         chrome_options = webdriver.ChromeOptions()
         chrome_options.add_argument('--headless')
+        #with proxy code doesnt work.
+        #chrome_options.add_argument(f'--proxy-server={os.getenv("PROXY")}')
         self.driver = webdriver.Chrome(options=chrome_options)
         self.url = os.getenv("INPUT_URL")
-        self.driver.get(self.url)
         self.data = []
 
     def get_data(self):
+        quotes = WebDriverWait(self.driver, 20).until(EC.presence_of_all_elements_located((By.XPATH, '//div[@id="quotesPlaceholder"]/div[@class="quote"]')))
+        for quote in quotes:
+            quote_data = {'text': "", 'by': "", 'tags': []}
+            quote_data['text'] = quote.find_element(by=By.XPATH, value='./span[@class="text"]').text.strip('“').strip('”')
+            quote_data['by'] = quote.find_element(by=By.XPATH, value='.//small[@class="author"]').text
+            quote_tags = quote.find_elements(by=By.XPATH, value='./div[@class="tags"]/a')
+            for tag in quote_tags:
+                quote_data['tags'].append(tag.text)
+            self.data.append(quote_data)
+
+    def next_page(self):
+        #delay clicking button - more human behaviour
+        sleep(3)
+        try:
+            self.driver.find_element(By.XPATH, '//li[@class="next"]/a').click()
+        except:
+            print('That was the last page.')
+            return False
+
+    def main(self):
+        self.driver.get(self.url)
+        print('First page.')
         while True:
-            quotes = WebDriverWait(self.driver, 20).until(EC.presence_of_all_elements_located((By.XPATH, '//div[@id="quotesPlaceholder"]/div[@class="quote"]')))
-            print(len(quotes))
-            for quote in quotes:
-                quote_data = {'text': "", 'by': "", 'tags': []}
-                quote_data['text'] = quote.find_element(by=By.XPATH, value='./span[@class="text"]').text.strip('“').strip('”')
-                quote_data['by'] = quote.find_element(by=By.XPATH, value='.//small[@class="author"]').text
-                quote_tags = quote.find_elements(by=By.XPATH, value='./div[@class="tags"]/a')
-                for tag in quote_tags:
-                    quote_data['tags'].append(tag.text)
-                print(quote_data)
-                self.data.append(quote_data)
-            #if next cant be clicked it means that's last page
-            try:
-                self.driver.find_element(By.XPATH, '//li[@class="next"]/a').click()
-            except:
+            self.get_data()
+            if self.next_page() == False:
                 write_to_jsonl(os.getenv("OUTPUT_FILE"), self.data)
                 break
-            #self.options = self.driver.find_elements_by_xpath('//ul[@aria-expanded="true"]/li')
+            print('Next page.')
 
 
 Scraping = Scraping()
-Scraping.get_data()
+Scraping.main()
--- test.py
+++
@@ -1,7 +0,0 @@
-    options.add_argument("--headless=new")
-    options.add_argument("--no-sandbox")
-    options.add_argument("--ignore-ssl-errors=true")
-    options.add_argument("--ignore-certificate-errors")    
-    
-    options.binary_location="C:/Projects/quotesscraper/chromedriver.exe"
-    driver = webdriver.Chrome('C:/PATH_TO/chromedriver_win32/chromedriver.exe',options=options)
